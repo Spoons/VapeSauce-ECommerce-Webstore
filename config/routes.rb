8d9bee0c7c63e572bca55f6c2dbcceb2030bbe64
meowman <mciocio1@uncc.edu>
2017-03-28 20:21:22 +0000

    added basket

--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,7 @@ Rails.application.routes.draw do
     get "product/:permalink", to: "products#show", as: "product"
     post "product/:permalink", to: "products#buy", as: "buy"
     root to: "products#index"
+    post "product/:permalink", to: "products#buy"
+    get "basket", to: "orders#show"
+
 end
