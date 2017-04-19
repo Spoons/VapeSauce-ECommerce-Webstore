cba35e29df3b9c76defcfdbc2094b0d88f04efe3
meowman <mciocio1@uncc.edu>
2017-04-19 22:18:09 +0000

    lots of css

--- spec/customer_views_a_product_spec.rb
+++ spec/customer_views_a_product_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper.rb'
 
-feature "Customer views product" do
+feature"Customer views product" do
     scenario "Customer sucessfully navigates to the products page" do
         visit home_path
         click_link "Enter, ye noble gentlesir"
